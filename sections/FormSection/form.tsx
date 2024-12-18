e9458d543fdff5a2dd3bc621c09009a39af08bcc
Smart-UI <dev@smart-ui.pro>
2024-12-18 16:34:28 +0200

    add loader and fix faq

--- sections/FormSection/form.tsx
+++ sections/FormSection/form.tsx
@@ -1,94 +1,103 @@
-import React, {FC, InputHTMLAttributes, useState, useEffect} from 'react';
+import React, { FC, InputHTMLAttributes, useState, useEffect } from 'react';
 import Image from 'next/image';
 import { useInView } from 'react-intersection-observer';
-import styles from './formSection.module.scss'
-import {DeepRequired, FieldErrorsImpl, FieldValues, GlobalError, useForm} from "react-hook-form";
+import styles from './formSection.module.scss';
+import { useForm } from 'react-hook-form';
 
 export const Form = () => {
-    const { ref, inView } = useInView({ threshold: 0.1 });
-    const [hasBeenVisible, setHasBeenVisible] = useState(false);
+  const { ref, inView } = useInView({ threshold: 0.1 });
+  const [hasBeenVisible, setHasBeenVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
 
-    useEffect(() => {
-        if (inView) {
-          setHasBeenVisible(true);
-        }
-    }, [inView]);
+  useEffect(() => {
+    if (inView) setHasBeenVisible(true);
+  }, [inView]);
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const [isLoading, setIsLoading] = useState(false);
-    const onSubmit = async (data:any) => {
-        try {
-            setIsLoading(true)
-            const response = await fetch('/api/contact', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data),
-            });
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
 
-            if (!response.ok) {
-                const error = await response.json();
-                throw new Error(error.message || 'Something went wrong');
-            }
+  const onSubmit = async (data: any) => {
+    try {
+      setIsLoading(true);
+      setIsSuccess(false);
+      const response = await fetch('/api/contact', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      });
 
-            const result = await response.json();
-            console.log('Message sent successfully:', result);
-        } catch (error:any) {
-            alert(error?.message)
-            console.error('Error while sending message:', error.message);
-        }
-        setIsLoading(false)
-    };
-    const formProps = {register ,errors }
-    return <section ref={ref} id='form' className={`${styles.formSection} ${hasBeenVisible ? 'visible' : ''}`}>
-        <div className='container'> 
-            <div className={styles.wrapper}>
-                <div className={styles.block}>
-                    <Image src='/hero_bg.jpg' alt="Фон з дівчиною" width={2400} height={1600} className={styles.heroBg} />
-                    <h2 ref={ref}  className={`${hasBeenVisible ? 'visible' : ''}`}>Зв'яжіться з нами</h2>
-                    <p>
-                        Ми раді отримати ваше повідомлення! Будь ласка, заповніть форму нижче, і наша команда зв'яжеться з вами якнайшвидше
-                    </p>
-                </div>
+      if (!response.ok) throw new Error('Помилка відправки форми');
 
-                <div  className={styles.form}>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                            <InputLine  id={"name"} label={"Твоє ім'я:"} placeholder="Ім'я"  formProps={formProps} autoComplete="name" />
-                            <InputLine id={"telegram"} label={"Твій нік у телеграм:"} placeholder="@Name"  formProps={formProps} autoComplete="telegram-username"/>
-                            <InputLine id={"phone"} label={"Твій номер телефону:"}   type="tel"
-                                    inputMode="numeric"
-                                    pattern="^\+?[0-9]*$"
-                                    autoComplete="tel"
-                                    placeholder="+380763125232" formProps={formProps}/>
+      setIsSuccess(true);
+      reset();
 
-                        <button className={styles.button} disabled={isLoading}  type="submit"> 
-                            Відправити  
+      setTimeout(() => {
+        setIsSuccess(false);
+      }, 10000);
+    } catch (error: any) {
+      alert(error?.message || 'Щось пішло не так');
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-                            <Image src='/elements/arrowRight.svg' alt="Стрілка вправо" width={24} height={24} />
-                        </button>
-                    </form>
-                </div>
-            </div>
+  const formProps = { register, errors };
+
+  return (
+    <section ref={ref} id="form" className={`${styles.formSection} ${hasBeenVisible ? 'visible' : ''}`}>
+      <div className="container">
+        <div className={styles.wrapper}>
+          <div className={styles.block}>
+            <Image src="/hero_bg.jpg" alt="Фон з дівчиною" width={2400} height={1600} className={styles.heroBg} />
+            <h2 className={`${hasBeenVisible ? 'visible' : ''}`}>Зв'яжіться з нами</h2>
+            <p>
+              Ми раді отримати ваше повідомлення! Будь ласка, заповніть форму нижче, і наша команда зв'яжеться з вами якнайшвидше
+            </p>
+          </div>
+
+          <div className={styles.form}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <InputLine id="name" label="Твоє ім'я:" placeholder="Ім'я" formProps={formProps} autoComplete="name" />
+              <InputLine id="telegram" label="Твій нік у телеграм:" placeholder="@Name" formProps={formProps} autoComplete="telegram-username" />
+              <InputLine id="phone" label="Твій номер телефону:" type="tel" inputMode="numeric" autoComplete="tel" placeholder="+380763125232" formProps={formProps} />
+
+              <button className={styles.button} disabled={isLoading} type="submit">
+                {isLoading ? (
+                  <>
+                    Відправка...
+                    <Image src="/elements/loader.svg" alt="Loading" width={20} height={20} className={styles.spinner} />
+                  </>
+                ) : (
+                  <>
+                    Відправити
+                    <Image src="/elements/arrowRight.svg" alt="Стрілка вправо" width={24} height={24} />
+                  </>
+                )}
+              </button>
+            </form>
+
+            {isSuccess && <div className={styles.successMessage}>Форму успішно відправлено!</div>}
+          </div>
         </div>
-    
-    </section> 
+      </div>
+    </section>
+  );
 };
 
 type TInputProps = {
-    formProps:{
-        register: any
-        errors: any
-    },
-    id:string,
-    label:string
-    required?:boolean
-}& InputHTMLAttributes<HTMLInputElement>;
-const InputLine:FC<TInputProps> = ({formProps , id  , label , required=true ,...rest   })=>{
-    const hasError = formProps.errors?.[id]
-    return <label className={styles.label} >
-      <span> {label}</span>
-        <input {...formProps.register(id, {required})   } {...rest}/>
-        {formProps.errors?.[id] && <div style={hasError?{color:"red"}:{}} className={styles.errorMessage}>Це поле є обов'язковим для заповнення</div>}
+  formProps: { register: any; errors: any };
+  id: string;
+  label: string;
+  required?: boolean;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+const InputLine: FC<TInputProps> = ({ formProps, id, label, required = true, ...rest }) => {
+  const hasError = formProps.errors?.[id];
+  return (
+    <label className={styles.label}>
+      <span>{label}</span>
+      <input {...formProps.register(id, { required })} {...rest} />
+      {hasError && <div className={styles.errorMessage}>Це поле є обов'язковим для заповнення</div>}
     </label>
-}
+  );
+};
